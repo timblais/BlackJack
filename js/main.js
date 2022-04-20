4d730b3224abcf448129eb9a4a6c813288301821
Tim Blais <tblais189@gmail.com>
2022-04-20 19:35:36 -0400

    completed objects for dealer and player with relevant methods. completed basic css styling. next steps: betting functionality

--- js/main.js
+++ js/main.js
@@ -1,35 +1,76 @@
 /* NEXT STEPS:
-- update older functions to incorporate new dealer object properties for dealer value storage
-- update player to be object based
-- update new hand function to clear object properties
 - clean up general play functionality eg new hand/deal cards should be obvious as next steps
 - add betting and system to keep track of player bets
 */
 
+const player1 = {
+  name: "Player 1",
+  total: 0,
+  cardValues: [],
+  bust: false,
+  money: 0,
+  currentBet: 0,
 
-// basic shell of constructor for future add player objects
+  hit: function(){
+    // grabs deckID from storage and sets API url to draw 1 card
+    let deckId = localStorage.getItem('DeckID')
+    const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
 
-class CreatePlayer{
-  constructor(name,money,curBet){
-    this.name = name
-    this.money = money
-    this.currentBet = curBet
-    this.currentHand = ''
-  }
-  bet(){
-   this.currentBet = prompt()
-   this.money -= this.currentBet 
-  }
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          console.log(data)
+          // Locally stores cards remaining and adds value to DOM
+          localStorage.setItem('cardsRemaining', data.remaining)
+          document.querySelector('#deckRemaining').innerText = localStorage.getItem('cardsRemaining')
 
-  hit(){
+          // Creates new img element and adds the card drawn to player 1's hand
+          let newCard = document.createElement('img')
+          newCard.classList.add("addedCard")
+          newCard.setAttribute('src', data.cards[0].image)
+          document.getElementById('p1Cards').appendChild(newCard)
 
-  }
+          // Adds value of new card to player1 object cardvalues array
+          player1.cardValues.push(convertToNum(data.cards[0].value))
+          player1.calcTotal()
+        })
+        .catch(err => {
+          console.log(`error ${err}`)
+      });
+    
+    
+  },
 
-  stand(){
+  stand: function(){
+    return dealer.playTurn()
+  },
+  
+  calcTotal: function(){
+    // Calculate current total
+    player1.total = player1.cardValues.reduce((a,c) => a + c)
+    // if less than 21, display total in the DOM
+    if(player1.total <= 21){
+      document.querySelector('#p1Value').innerText = player1.total
+    }else if (player1.total > 21){
+      // if over 21, determine if there is an ace. If so, replace value with 1 and rerun this method
+      if(player1.cardValues.includes(11)){
+        let index = player1.cardValues.findIndex(x => x > 10)
+        player1.cardValues.splice(index, 1, 1)
+        return player1.calcTotal()
+      // if no ace, player busts. Display total in DOM and run bust method
+      }else{
+        document.querySelector('#p1Value').innerText = player1.total
+        player1.bust = true
+        return player1.playerBust()
+      }
+    }
+  },
 
-  }
+  playerBust: function(){
+    document.querySelector("#result").innerText = `${this.name} busts!`
+    // add functions here to clear the player's bet and restrict access to hit and stand buttons
+  },
 }
-
 
 const dealer = {
   total: 0, //used to store dealer's total card value
@@ -56,27 +97,36 @@ const dealer = {
             
             // adds newly drawn card value to dealer cardValues and updates total, then updates total display in the DOM
             this.cardValues.push(Number(convertToNum(data.cards[0].value)))
-            this.total = this.cardValues.reduce((a,c => a + c))
+            this.total = this.cardValues.reduce((a,c) => a + c)
             document.querySelector('#dValue').innerText = this.total
+          })
+          .then(data => {
+            this.playTurn() // runs playTurn function
           })
             .catch(err => {
               console.log(`error ${err}`)
           });
-    return this.playTurn() // runs playTurn function
+
   },
 
   // playTurn function performs dealer total calculations and determines next step in dealer's turn
   playTurn: function(){
+    document.querySelector(".cardBack").hidden = true
+    document.querySelector('#dcard2').hidden = false
+    
+    this.total = this.cardValues.reduce((a,c) => a + c)
+    document.querySelector('#dValue').innerText = this.total
     if(this.total > 16 && this.total <= 21){
       return calcWinner() // returns calcWinner function if dealer is within stand parameters
     }else if(this.total < 16){
       return this.hit() // tells dealer to hit if under 16
     }else if(this.total > 21){ // if dealer is over 21, checks to see if the hand was soft with an ace included. If an ace exists on the cardValues property, replaces it with a 1, updates the total, and reruns this function. If no ace, declares bust and runs calcWinner function
       if(this.cardValues.includes(11)){
-        let index = this.cardValues.findIndex(x => x > 10)
-        this.cardValues.splice(index, 1, 1)
-        this.total = this.cardValues.reduce((a,c => a + c))
-        document.querySelector('#dValue').innerText = this.total
+        this.cardValues = this.cardValues.filter(x => x < 11)
+        this.cardValues.push(1)
+
+        // let index = this.cardValues.findIndex(x => x > 10)
+        // this.cardValues.splice(index, 1, 1)
         return this.playTurn() 
       }else{
         this.bust = true
@@ -87,14 +137,9 @@ const dealer = {
   
 }
     
- 
-
-
 document.querySelector('#deckRemaining').innerText = localStorage.getItem('cardsRemaining')
 
 document.querySelector('#deck').addEventListener('click', shuffleDeck)
-
-//Need to go back and add card count to other functions eventually and have them update the remaining cards. Probably change from input to other element so it won't be confusing and save between referesh. Should add the deck and card count to local storage
 
 function shuffleDeck(){
   fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
@@ -113,38 +158,43 @@ function shuffleDeck(){
 
 document.querySelector('#deal').addEventListener('click', dealCards)
 
-// Need to add clear to the dealCards function to remove img tags added via hit on previous hand
-
 function dealCards(){
+  // grabs deckID from storage and sets API url to draw 4 cards
   let deckId = localStorage.getItem('DeckID')
-  
   const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        // Locally stores cards remaining and adds value to DOM
+        localStorage.setItem('cardsRemaining', data.remaining)
+        document.querySelector('#deckRemaining').innerText = localStorage.getItem('cardsRemaining')
+
+        // Add Card Images to the DOM. Dealer's second card is hidden
         document.querySelector('#p1card1').src = data.cards[0].image 
         document.querySelector('#dcard1').src = data.cards[1].image
         document.querySelector('#p1card2').src = data.cards[2].image 
         document.querySelector('#dcard2').src = data.cards[3].image
         document.querySelector('#dcard2').hidden = true
 
+        // Creates face down card image for dealer's second card
         let backCard = document.createElement('img')
         backCard.classList.add("addedCard", "cardBack")
         backCard.setAttribute('src', "images/cardback.png")
         document.getElementById('dCards').appendChild(backCard)
 
+        // Adds player1's cards to cardvalues and total properties on object, then adds total to DOM
+        player1.cardValues.push(convertToNum(data.cards[0].value))
+        player1.cardValues.push(convertToNum(data.cards[2].value))
+        player1.total = player1.cardValues.reduce((a,c) => a + c)
+        document.querySelector('#p1Value').innerText = player1.total
 
-        localStorage.setItem('cardsRemaining', data.remaining)
-        document.querySelector('#deckRemaining').innerText = localStorage.getItem('cardsRemaining')
-        localStorage.setItem("hiddenValue", data.cards[3].value)
-
-        let player1Val = convertToNum(data.cards[0].value) + convertToNum(data.cards[2].value)
-        document.querySelector('#p1Value').innerText = player1Val
-        let dealerVal = convertToNum(data.cards[1].value) 
-        document.querySelector('#dValue').innerText = dealerVal
-        
+        // Adds dealer's cards to cardvalues and total properties on object, then sets total to only the first card
+        dealer.cardValues.push(convertToNum(data.cards[1].value))
+        dealer.cardValues.push(convertToNum(data.cards[3].value))
+        dealer.total = dealer.cardValues[0]
+        document.querySelector('#dValue').innerText = dealer.total
       })
       .catch(err => {
           console.log(`error ${err}`)
@@ -165,137 +215,49 @@ function convertToNum(val){
   }
 }
 
-document.querySelector('#p1Hit').addEventListener('click', p1Hit)
-
-function p1Hit(){
-  let deckId = localStorage.getItem('DeckID')
-  let result
-
-  const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        localStorage.setItem('cardsRemaining', data.remaining)
-        document.querySelector('#deckRemaining').innerText = localStorage.getItem('cardsRemaining')
-
-        let newCard = document.createElement('img')
-        newCard.classList.add("addedCard")
-        newCard.setAttribute('src', data.cards[0].image)
-        document.getElementById('p1Cards').appendChild(newCard)
-
-        let player1Val = Number(document.querySelector('#p1Value').innerText)
-        let cardValue = convertToNum(data.cards[0].value)
-
-        //this works to adjust ace when an 11 would cause bust, need to add functionality to display bust in DOM, as well as display 21.
-        if(player1Val + cardValue > 21){
-          if(data.cards[0].value == 'ACE'){
-            player1Val += 1
-            document.querySelector('#p1Value').innerText = player1Val
-          }else{
-            document.querySelector('#p1Value').innerText = (player1Val + cardValue)
-            console.log("Bust")
-            result = "Player 1 busts!"
-            document.querySelector("#result").innerText = result
-          }
-        }else{
-          document.querySelector('#p1Value').innerText = (player1Val + cardValue)
-        }
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
+document.querySelector('#p1Hit').addEventListener('click', player1.hit)
 
 function newHand(){
+  
+  // actions to clear display of the DOM
   document.querySelectorAll(".addedCard").forEach(element => element.remove())
-  document.querySelectorAll(".starterCard").forEach(element => element.setAttribute('src', ""))
   document.querySelectorAll(".starterCard").forEach(element => element.setAttribute('src', ""))
   document.querySelectorAll(".values").forEach(element => element.innerText = "")
   document.querySelector("#result").innerText = "Result: "
+
+  // actions to clear object property values:
+  dealer.total = 0
+  dealer.cardValues = []
+  dealer.bust = false
+  player1.total = 0
+  player1.cardValues = []
+  player1.bust = false
+  player1.currentBet = 0
+
 }
 
 document.querySelector("#newHand").addEventListener("click", newHand)
 
-
-function stand(){
-  document.querySelector(".cardBack").remove()
-  document.querySelector('#dcard2').hidden = false
-  
-  let hiddenVal = convertToNum(localStorage.getItem("hiddenValue"))
-  let dealerVal = Number(document.querySelector('#dValue').innerText)
-  document.querySelector('#dValue').innerText = dealerVal + hiddenVal
-
-  dealerVal = dealerVal + hiddenVal
-
-  return dealerVal > 16 ? calcWinner() : dealerHit()
-
-}
-
-document.querySelector(".stand").addEventListener("click", stand)
+document.querySelector(".stand").addEventListener("click", player1.stand)
 
 function calcWinner(){
-  let p1Val = Number(document.getElementById('p1Value').innerText)
-  let dVal = Number(document.getElementById('dValue').innerText)
   let result
 
-  if(p1Val > dVal){
+  if(player1.total > dealer.total){
     result = "Player 1 wins!"
-  }else if(p1Val == dVal){
+    player1.money += player1.currentBet * 2
+    //add money value to DOM
+  }else if(dealer.bust == true){
+    result = "Dealer Busts! Player 1 wins!"
+    player1.money += player1.currentBet * 2
+    //add money value to DOM
+  }else if(player1.total == dealer.total){
     result = "Draw"
-  }else if(p1Val < dVal){
+    player1.money += player1.currentBet
+    //add money value to DOM
+  }else if(player1.total < dealer.total){
     result = "Dealer wins!"
   }
   document.querySelector("#result").innerText = result
 }
 
-function dealerHit(){
-  let deckId = localStorage.getItem('DeckID')
-  let result
-  let dealerVal = Number(document.querySelector('#dValue').innerText)
-
-  if(dealerVal > 16 && dealerVal <=21){
-    return calcWinner()
-  }else{
-    const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-
-    fetch(url)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-          console.log(data)
-          localStorage.setItem('cardsRemaining', data.remaining)
-          document.querySelector('#deckRemaining').innerText = localStorage.getItem('cardsRemaining')
-  
-          let newCard = document.createElement('img')
-          newCard.classList.add("addedCard")
-          newCard.setAttribute('src', data.cards[0].image)
-          document.getElementById('dCards').appendChild(newCard)
-  
-          let dealerVal = Number(document.querySelector('#dValue').innerText)
-          let cardValue = convertToNum(data.cards[0].value)
-  
-          //this works to adjust ace when an 11 would cause bust, need to add functionality to display bust in DOM, as well as display 21.
-          if(dealerVal + cardValue > 21){
-            if(data.cards[0].value == 'ACE'){
-              dealerVal += 1
-              document.querySelector('#dValue').innerText = dealerVal
-              //currently only accounts for the last card being an ace. Probably need to store the first two cards in local storage or on a dealer object. First need to create dealer object. maybe having hit/stand as object methods would be cleaner in the end
-            }else{
-              document.querySelector('#dValue').innerText = (dealerVal + cardValue)
-              console.log("Bust")
-              result = "Dealer busts! Player 1 wins!"
-              document.querySelector("#result").innerText = result
-            }
-          }else{
-            document.querySelector('#dValue').innerText = (dealerVal + cardValue)
-            return dealerHit()
-          }
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
-  }
-
-  
-}
